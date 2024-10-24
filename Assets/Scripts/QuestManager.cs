cc41c73ddff60f8124f25c4d01333d76feeebc78
moomin <amily8050@gmail.com>
2024-10-24 12:52:58 +0900

    QuestManager 수정
    Quest 종류 구분하여 출력

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -42,13 +42,24 @@ public class QuestManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
     }
-    // Start is called before the first frame update
+
     void Start()
     {
         for (int i = 0; i < Quests.Count; i++)
         {
             QuestDataSO quest = Quests[i];
             Debug.Log($"Quest {i + 1} - {quest.QuestName} (최소 레벨 {quest.QuestRequiredLevel})");
+
+            if (quest is MonsterQuestDataSO)
+            {
+                MonsterQuestDataSO monsterQuest = (MonsterQuestDataSO)quest;
+                Debug.Log($"{monsterQuest.MonsterName}을(를) {monsterQuest.SweepMonsterCount}마리 소탕");
+            }
+            else if (quest is EncounterQuestDataSO)
+            {
+                EncounterQuestDataSO encounterQuest = (EncounterQuestDataSO)quest;
+                Debug.Log($"{encounterQuest.encounterName}과 대화하기");
+            }
         }
     }
 }
